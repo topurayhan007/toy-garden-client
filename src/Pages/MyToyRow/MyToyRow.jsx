7a825551f208879008b0fa23e4a867bcbc87cac0
topurayhan007 <topurayhan007@gmail.com>
2023-05-20 03:09:32 +0600

    My toys data shown done

--- src/Pages/MyToyRow/MyToyRow.jsx
+++ src/Pages/MyToyRow/MyToyRow.jsx
@@ -1,44 +1,65 @@
+/* eslint-disable react/prop-types */
 import { RxUpdate } from "react-icons/rx";
 import { AiFillDelete } from "react-icons/ai";
+import Rating from "react-rating";
+import { BsStarFill, BsStar } from "react-icons/bs";
 
-const MyToyRow = () => {
+const MyToyRow = ({ toy }) => {
+  const {
+    _id,
+    name,
+    subCategory,
+    rating,
+    pictureURL,
+    price,
+    quantity,
+    description,
+  } = toy;
   return (
-    <tr className="">
+    <tr className=" ">
       <td>
         <div className="flex items-center gap-3 text-lg mx-0">
           <div className="avatar">
             <div className="mask mask-circle w-24 h-24">
-              <img
-                src="https://c0.wallpaperflare.com/preview/878/814/409/guardians-of-the-galaxy-groot-ceramic-figurine.jpg"
-                alt=""
-              />
+              <img src={pictureURL} alt="" />
             </div>
           </div>
           <div>
-            <div className="font-bold mr-0 text-xl text-[#2F0743]">
-              Obi-Wan Kenobi Action Figure
-            </div>
+            <div className="font-bold mr-0 text-xl text-[#2F0743]">{name}</div>
+            <p className="mt-1 bg-transparent hover:bg-transparent text-orange-500 normal-case text-base font-bold flex justify-start items-center">
+              <Rating
+                readonly="true"
+                className="me-1"
+                initialRating={rating}
+                emptySymbol={<BsStar className="" />}
+                fullSymbol={<BsStarFill className="" />}
+              />
+              <span className="text-black">{rating}</span>
+            </p>
             <div className="text-purple-800 bg-purple-50 w-max px-5 rounded-full py-1 mt-2">
-              Marvel
+              {subCategory}
             </div>
           </div>
         </div>
       </td>
       <td>
-        <p className="font-medium text-lg text-center">$24.99</p>
+        <p className="font-medium text-lg text-center">${price}</p>
       </td>
       <td>
-        <p className="font-medium text-lg text-center">59</p>
+        <p className="font-medium text-lg text-center">{quantity}</p>
       </td>
 
-      <td>
+      <td className="min-w-[12rem] max-w-[23rem] whitespace-normal mr-0">
         <div className="flex justify-left">
-          <p className="">The quick brown fox killed the bear</p>
+          <p className="">{description}</p>
         </div>
       </td>
       <td>
         <div className="flex items-center space-x-3 text-lg justify-center">
-          <button className="btn normal-case text-lg rounded-full font-medium bg-green-200 hover:bg-green-300 hover:border-emerald-400 border-2 border-emerald-400 text-emerald-700">
+          <button
+            onClick={_id}
+            className="btn normal-case text-lg rounded-full font-medium bg-green-200 hover:bg-green-300 hover:border-emerald-400 border-2 border-emerald-400 text-emerald-700"
+          >
             Update <RxUpdate className="ms-2 text-emerald-700" />
           </button>
           <button className="btn normal-case text-lg rounded-full font-medium bg-red-200 hover:bg-red-300 hover:border-rose-400 border-2 border-rose-400 text-rose-700">
--- src/Pages/MyToys/MyToys.jsx
+++ src/Pages/MyToys/MyToys.jsx
@@ -1,41 +1,65 @@
+import { useContext, useEffect, useState } from "react";
 import MyToyRow from "../MyToyRow/MyToyRow";
-
+import { AuthContext } from "../../providers/AuthProvider";
 
 const MyToys = () => {
+  const { user } = useContext(AuthContext);
+  const [userToys, setUserToys] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url = `https://toy-garden-server.vercel.app/my-toys/${user?.email}`;
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setUserToys(data);
+        setLoading(false);
+      });
+  }, [url]);
+
   return (
     <div className="my-24 mx-auto max-w-7xl text-center">
       <h2 className="font-bold text-5xl text-[#2F0743] mb-8">My Added Toys</h2>
-      <div className="overflow-x-auto w-full border border-purple-900 border-t-[10px]  rounded-xl">
-        <table className="table w-full">
-          {/* head */}
-          <thead>
-            <tr>
-              <th className="bg-[#f8efff] border-b-2 border-purple-100 normal-case text-xl w-36 text-center">
-                Name & Info
-              </th>
-              <th className="bg-[#f8efff] border-b-2 border-purple-100 normal-case text-xl text-center">
-                Price
-              </th>
-              <th className="bg-[#f8efff] border-b-2 border-purple-100 normal-case text-xl text-center">
-                Quantity
-              </th>
-              <th className="bg-[#f8efff] border-b-2 border-purple-100 normal-case text-xl w-72 text-center">
-                Description
-              </th>
-              <th className="bg-[#f8efff] border-b-2 border-purple-100 normal-case text-xl text-center">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* Row */}
-            <MyToyRow></MyToyRow>
-            <MyToyRow></MyToyRow>
-            <MyToyRow></MyToyRow>
-            <MyToyRow></MyToyRow>
-          </tbody>
-        </table>
-      </div>
+      {loading === true ? (
+        <div className="my-14 flex justify-center">
+          <div
+            className="radial-progress animate-spin text-[#ea6802]"
+            style={{ "--value": 85 }}
+          ></div>
+        </div>
+      ) : (
+        <div className="overflow-x-auto w-full border border-purple-900 border-t-[10px] rounded-xl">
+          <table className="table w-full even:bg-slate-900 table-auto">
+            {/* head */}
+            <thead>
+              <tr>
+                <th className="bg-[#f8efff] border-b-2 border-purple-100 normal-case text-xl w-36 text-center">
+                  Name & Info
+                </th>
+                <th className="bg-[#f8efff] border-b-2 border-purple-100 normal-case text-xl text-center">
+                  Price
+                </th>
+                <th className="bg-[#f8efff] border-b-2 border-purple-100 normal-case text-xl text-center">
+                  Quantity
+                </th>
+                <th className="bg-[#f8efff] border-b-2 border-purple-100 normal-case text-xl text-center ">
+                  Description
+                </th>
+                <th className="bg-[#f8efff] border-b-2 border-purple-100 normal-case text-xl text-center">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+
+            <tbody className="">
+              {/* Row */}
+              {userToys.map((toy) => (
+                <MyToyRow key={toy._id} toy={toy}></MyToyRow>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
